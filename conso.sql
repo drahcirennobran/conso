1b5ae76bfc9fb8214ab87c69e4fc84622e1e25e9
Richard Narbonne <richard.narbonne@gmail.com>
2015-01-16 15:20:47 +0000

    déplacement dans batch

--- conso.sql
+++
@@ -1,8 +0,0 @@
-USE conso;
-drop table if exists conso;
-CREATE TABLE conso(
-       `idCompteur` INT,
-       `captureDate` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       `conso` INT,
-       KEY(captureDate)
-);
