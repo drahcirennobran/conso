6bdeb67ed36be009faca0c4394b377d809dea1d3
Richard Narbonne <richard.narbonne@gmail.com>
2015-02-09 15:23:03 +0100

    ajout

---
+++ web/consoInstantanee.py
@@ -0,0 +1,17 @@
+import urllib
+
+def application(environ, start_response):
+
+   #link = "http://192.168.0.102:31415/cumulus_i"
+   #f = urllib.urlopen(link)
+   #conso = f.read()
+
+   response_body = "[{\"conso\":%d}]"% 225;
+
+   status = '200 OK'
+   response_headers = [('Content-Type', 'application/json'),
+                       ('Content-Length', str(len(response_body)))]
+
+   start_response(status, response_headers)
+
+   return [response_body]
