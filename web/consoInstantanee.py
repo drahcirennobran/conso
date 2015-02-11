02ca997ec5a4625bbcc1dd8379c3ec26297c4d62
Richard Narbonne <richard.narbonne@gmail.com>
2015-02-11 11:12:03 +0100

    conso instantannee random

--- web/consoInstantanee.py
+++ web/consoInstantanee.py
@@ -2,11 +2,11 @@ import urllib
 
 def application(environ, start_response):
 
-   #link = "http://192.168.0.102:31415/cumulus_i"
-   #f = urllib.urlopen(link)
-   #conso = f.read()
+   link = "http://192.168.0.102:31415/cumulus_i"
+   f = urllib.urlopen(link)
+   conso = f.read()
 
-   response_body = "[{\"conso\":%d}]"% 225;
+   response_body = conso;
 
    status = '200 OK'
    response_headers = [('Content-Type', 'application/json'),
