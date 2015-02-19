2af491ba99b7a9c760e4388d3a35a173c91a92a6
Richard Narbonne <richard.narbonne@gmail.com>
2015-02-19 16:40:16 +0100

    ajout compteur chauffage

--- arduino/compteur/compteur.ino
+++ arduino/compteur/compteur.ino
@@ -3,11 +3,12 @@
 #include <Ethernet.h>
 
 #define PIN2INT 0
+#define PIN3INT 1
 #define PINLED 7
 
 int etat = HIGH;
-unsigned long mwh = 0;
-unsigned long lastChange;
+unsigned long mwh[25];
+unsigned long lastChange[2];
 
 byte mac[] = {
   0x90, 0xA2, 0xDA, 0x0E, 0xA1, 0x92
@@ -19,15 +20,14 @@ void setup() {
   Serial.begin(9600);
   pinMode(PINLED, OUTPUT);
   digitalWrite(PINLED, etat);
-
-  attachInterrupt(PIN2INT, tick, FALLING);
+  mwh[PIN2INT] = mwh[PIN3INT] = 0;
+  attachInterrupt(PIN2INT, tickPin2, FALLING);
+  attachInterrupt(PIN3INT, tickPin3, FALLING);
   Ethernet.begin(mac, ip);
   server.begin();
 }
 
 void loop() {
-  Serial.println(mwh);
-
   EthernetClient client = server.available();
   String line;
   boolean lastLineEmpty = false;
@@ -39,13 +39,22 @@ void loop() {
       if (isEmptyLine(line)) {
         lastLineEmpty = true;
       }
-      else if (line.indexOf("GET /mwh") != -1) {
-        client.println(String(mwh));
-        mwh = 0;
+      else if (line.indexOf("GET /cumulusmwh") != -1) {
+        client.println(String(mwh[PIN2INT]));
+        mwh[PIN2INT] = 0;
+      }
+      else if (line.indexOf("GET /chauffagemwh") != -1) {
+        client.println(String(mwh[PIN3INT]));
+        mwh[PIN3INT] = 0;
       }
       else if (line.indexOf("GET /cumulus_i") != -1) {
         static float numerateur = 60000;
-        int conso = numerateur / (millis() - lastChange);
+        int conso = numerateur / (millis() - lastChange[PIN2INT]);
+        client.println(String(conso));
+      }
+      else if (line.indexOf("GET /chauffage_i") != -1) {
+        static float numerateur = 60000;
+        int conso = numerateur / (millis() - lastChange[PIN3INT]);
         client.println(String(conso));
       }
       line = client.readStringUntil('\n');
@@ -55,12 +64,20 @@ void loop() {
   }
 }
 
-void tick(void) {
+void tickPin2(void) {
+  tick(PIN2INT);
+}
+
+void tickPin3(void) {
+  tick(PIN3INT);
+}
+
+void tick(int interrupNo) {
   //__|-|__ tick 90 ms :  status changed between 50 and 150 ms
-  if (millis() - lastChange > 200){// && millis() - lastChange < 800) {
+  if (millis() - lastChange[interrupNo] > 200) { // && millis() - lastChange < 800) {
     etat = !etat;
-    mwh ++;
-    lastChange = millis();
+    mwh[interrupNo] ++;
+    lastChange[interrupNo] = millis();
     digitalWrite(PINLED, etat);
   }
 }
@@ -70,9 +87,4 @@ boolean isEmptyLine(String line) {
   return line.charAt(0) == '\r' || line.charAt(0) == '\n';
 }
 
-unsigned long getMwh() {
-  static unsigned long tmp = mwh;
-  mwh = 0;
-  return tmp;
-}
 
